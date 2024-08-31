f777a0b6a4e14f2731dad13144ecf262756cd93e
Ashik-ch <ashikch7117@gmail.com>
2024-08-31 23:27:17 +0530

    second commit

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,21 +3,26 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { PagesRoutingModule } from './home/pages-routing.module';
 import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
 
-
+const Component = [
+  HomeComponent,
+  HeaderComponent,
+  AboutComponent,
+  FooterComponent
+]
 
 @NgModule({
   declarations: [
-    HomeComponent,
-    HeaderComponent
+    Component
   ],
   imports: [
     CommonModule,
     PagesRoutingModule,
   ],
   exports: [
-    HomeComponent,
-    HeaderComponent
+    Component
   ]
 })
 export class PagesModule { }
