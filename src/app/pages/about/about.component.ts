e7a27f4c166eaac7152c378c0a49fe924bd6a398
Ashik-ch <ashikch7117@gmail.com>
2024-09-08 23:16:07 +0530

    images

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
+  aboutPage: boolean = false;
+  constructor(private router: Router) { }
 
+  ngOnInit() {
+    const url = this.router.url;
+    this.aboutPage = url === '/about';
+  }
 }
